3f2c698f2c03757945104aad391d3eea4e865f5a
Kiriyuki <kiriyuki02@gmail.com>
2024-05-08 04:26:19 +0700

    feat: floating Player
    player is not working

--- src/app/components/TracksList.tsx
+++ src/app/components/TracksList.tsx
@@ -2,7 +2,7 @@ import { FlatList, FlatListProps, View } from 'react-native'
 import library from '@/assets/data/library.json'
 import { TracksListItem } from './TrackListItems'
 import { utilStyles } from '@/styles'
-import { Track } from 'react-native-track-player'
+import TrackPlayer, { Track } from 'react-native-track-player'
 
 export type TracksListProps = Partial<FlatListProps<Track>> & {
 	tracks: Track[]
@@ -11,8 +11,8 @@ const ItemDivider = () => (
 	<View style={{ ...utilStyles.itemSeparator, marginVertical: 9, marginLeft: 60 }} />
 )
 export const TracksList = ({ tracks, ...flatlistProps }: TracksListProps) => {
-	const handleTrackSelect = (track: Track) => {
-		console.log(track)
+	const handleTrackSelect = async (track: Track) => {
+		await TrackPlayer.load(track)
 	}
 	return (
 		<FlatList
